f2a432db0df4e513c1a015f6114cc160424595d0
trofimchuk <trofimchuk@itstep.academy>
2024-01-26 19:14:07 +0200

    Add Connection string

---
+++ NewsApi.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsApi.Infrastructure.Context
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext() : base() { }
+        public AppDbContext(DbContextOptions options) : base (options) { }
+
+        
+    }
+}
